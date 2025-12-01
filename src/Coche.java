cd23dbad44d8005fcb17a87a0c5dcef99deabbf5
David Arranz González <a25davidag@teis.intranet>
2025-12-01 19:02:43 +0100

    djfdj

--- src/Coche.java
+++ src/Coche.java
@@ -1,3 +1,50 @@
 public class Coche {
+    private String modelo;
+    private String marca;
+    private Motor motor;
 
+    public Coche(String modelo,String marca,Motor motor) {
+        this.modelo = modelo;
+        this.motor = motor;
+        this.marca = marca;
+    }
+
+    public Coche(String modelo, String marca) {
+        this.modelo = modelo;
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        motor = motor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void arrancar(){
+        System.out.println("Arrancamos el coche");
+        motor.encender();
+
+    }
+    public void detener() {
+        System.out.println("Detenemos el coche");
+        motor.apagr();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Espero que te lo pases bien!");
+
+        ArrayList<Coche> listaCoches = new ArrayList<>();
+
+        Motor diesel = new Motor(100,500);
+        Motor gasolina = new Motor(1232,2344);
+
+        Coche coche1 = new Coche("auris","toyota",diesel);
+        Coche coche2 = new Coche("c4","Citroen",diesel);
+        Coche coche3 = new Coche("Panda","Seat",gasolina);
+        Coche coche4 = new Coche("Aventador","Lamborgini",gasolina);
+
+
+        listaCoches.add(coche1);
+        listaCoches.add(coche2);
+        listaCoches.add(coche3);
+        listaCoches.add(coche4);
+
+
+        for (Coche coche: listaCoches){
+            if(coche.getMotor()== null){
+                coche.setMotor(gasolina);
+            }
+        }
+
+        for(Coche coche: listaCoches){
+            coche.arrancar();
+        }
+        for(Coche coche: listaCoches){
+            coche.detener();
+        }
 
     }
 }
